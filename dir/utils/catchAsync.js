011eda750312781b69054b9dad87959e3694f0dc
Chester <ochester4@gmail.com>
2026-02-11 09:52:00 +0100

    latest

--- dir/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = catchAsync;
-function catchAsync(fn) {
-    return (req, res, next) => {
-        // Call the async function and catch any errors
-        fn(req, res, next).catch(next); // Forward error to Express error handler
-    };
-}
